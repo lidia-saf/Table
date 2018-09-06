2e727d9f25b9268627d76992b9e8bd62757d6ee9
lidia-saf <lidiia.safiullina@yandex.ru>
2018-09-06 21:32:22 +0300

    initial commit

---
+++ table-app/src/Application.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
+
+class Application extends Component {
+    render() {
+        return(
+            <div>
+                здесь будет таблица
+            </div>
+        )
+    }
+}
+
+export default Application;
